17c11046099099994364dfacfafd94aa52833c01
AI-Gio <wreckitsnoofs@gmail.com>
2021-09-29 21:34:14 +0200

    Better wander functions added to TriageRationeel

--- TriageRationeel/agents.py
+++ TriageRationeel/agents.py
@@ -9,8 +9,10 @@ class Medic(Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         self.brancard = []
-        self.path = []
+        self.path = [(0, 0), (0, 1), (1, 0)]
         self.known_p = []
+        self.current_path = ()
+        self.previous_location = None
         self.emotional_state = 100
         self.pickedup = False
 
@@ -35,34 +37,61 @@ class Medic(Agent):
             self.pickedup = True
             self.model.grid.remove_agent(patient)
 
-    def wander(self, surround):
+    def wander_choice_maker(self, locations, counter=0):
+        choices = {}
+        for pos in locations:
+            print(list(pos)[-1])
+            surraw = [(list(pos)[-1][0], list(pos)[-1][1]+1), (list(pos)[-1][0], list(pos)[-1][1]-1),
+                      (list(pos)[-1][0]+1, list(pos)[-1][1]), (list(pos)[-1][0]-1, list(pos)[-1][1])]
+            sur = [i for i in surraw if (0 <= i[0] < self.model.width) and (0 <= i[1] < self.model.height)]
+            if self.previous_location in sur and len(sur) > 1:
+                sur.remove(self.previous_location)
+            for loc in sur:
+                if loc in pos:
+                    continue
+                sursuround = self.model.grid.get_neighborhood(loc, moore=False, include_center=True)
+                score = 0
+                for surloc in sursuround:
+                    if surloc not in self.path:
+                        score += 1
+
+                path = list(pos)
+                path.append(loc)
+                choices[tuple(path)] = score
+
+
+        possible_choices = [k for k, v in choices.items() if v == max(choices.values())]
+
+        if (len(possible_choices) > 1 and counter < 3) and max(choices.values()) < 1:
+            possible_choices = self.wander_choice_maker(possible_choices, counter+1)
+
+        return possible_choices
+
+    def wander(self):
         """
         Medic wanders through field mostly away from base and tries to explore yet haven't found locations
         """
         # todo: gaat opzoek naar vakjes die nog niet bezocht zijn
-        choices = {}
-        for loc in surround:
-            sursuround = self.model.grid.get_neighborhood(loc, moore=False, include_center=True)
-            score = 0
-            for surloc in sursuround:
-                if surloc not in self.path:
-                    score += 1
-            choices[loc] = score
+        if len(self.current_path) < 1:
+            # get all best choices, shuffle and pick one randomly
+            original_path = [[]]
+            original_path[0].append(self.pos)
+            possible_choices = self.wander_choice_maker(original_path)
+            self.current_path = list(random.choice(possible_choices)[1::])
 
-        # get all best choices, shuffle and pick one randomly
-        possible_choices = [k for k, v in choices.items() if v == max(choices.values())]
-        choice = random.choice(possible_choices)
-        self.model.grid.move_agent(self, choice)
-
+        self.previous_location = self.pos
+        self.model.grid.move_agent(self, self.current_path[0])
         # add new locations to database of known locations
-        new_loc = self.model.grid.get_neighborhood(choice, moore=False, include_center=True)
+        new_loc = self.model.grid.get_neighborhood(self.current_path[0], moore=False, include_center=True)
         self.path = self.path + list(set(new_loc) - set(self.path))  # removes duplicates
+        del self.current_path[0]
 
     def walk(self, point):
         """
         Medic walks straight to a coordinate
         :return:
         """
+        self.current_path = ()
         x, y = self.pos
         if x > point[0]:
             # moet naar links
@@ -88,6 +117,7 @@ class Medic(Agent):
         Uses shortest path alg to return to base to return patient
         :return:
         """
+        self.current_path = ()
         x, y = self.pos
         if self.pos[0] > 0:
             self.model.grid.move_agent(self, (x-1,y))
@@ -134,14 +164,14 @@ class Medic(Agent):
             if self.brancard[0].health == 0:
                 print("Patient died")
                 self.brancard = []
-                self.wander(nb_coords)
+                self.wander()
                 self.pickedup = False
 
         elif len(self.known_p) > 0: # als er locaties van patient zijn onthouden
             self.walk(self.known_p[0][0])
 
         if len(self.brancard) == 0 and len(self.known_p) == 0: # als brancard leeg is en er zijn geen bekende plekken van patienten
-            self.wander(nb_coords)
+            self.wander()
             self.pickedup = False
 
 
